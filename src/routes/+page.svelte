b51256b23dac90fce49bf7a88bbc44d6a610daac
suryanshu-09 <suryanshusaini@gmail.com>
2025-05-21 01:23:31 +0530

    debug vercel deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
 	import Main from '../components/Main.svelte';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		if (typeof window !== 'undefined' && window.particlesJS) {
-			window.particlesJS.load('particles-js', '/assets/particles.json', () => {
-				console.log('Particles loaded');
-			});
-		}
-	});
 </script>
 
 <Main />
